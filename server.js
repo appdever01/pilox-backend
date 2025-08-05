48d9af5942ddd2a149b0ee0b45e8907870945c07
appdever01 <appdever01@gmail.com>
2025-08-05 17:00:00 +0100

    update server.js

--- server.js
+++ server.js
@@ -22,6 +22,7 @@ const corsOptions = {
     'https://pilox.com.ng',
     'http://localhost:9000',
     'http://localhost:3000',
+    'http://localhost:3001',
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
